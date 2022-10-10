f1bbbe84bd42317edbc8a7c6b0ef99473e8198e7
fullmetal88 <igor.orszanski@sofomo.com>
2022-10-10 16:34:52 +0200

    add rollup

---
+++ rollup.config.ts
@@ -0,0 +1,45 @@
+import typescript from 'rollup-plugin-typescript2'
+import postcss from 'rollup-plugin-postcss'
+import dts from "rollup-plugin-dts"
+import del from 'rollup-plugin-delete'
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/CodeInput.cjs',
+        format: "cjs",
+        sourcemap: true
+      },
+      {
+        file: 'dist/CodeInput.esm',
+        format: "esm",
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true
+      }),
+      postcss({
+        plugins: []
+      })
+    ]
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [
+      {
+        file: "dist/CodeInput.d.ts",
+        format: "esm"
+      }
+    ],
+    plugins: [
+      dts(),
+      del({
+        targets: 'dist/types', hook: 'generateBundle'
+      })
+    ],
+  },
+];
